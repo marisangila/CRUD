18e421ecac7e034635481139ee3f30ebe47a835a
name_do_github <email_github@gmail.com>
2022-09-25 15:55:37 -0300

    create login vlaidation

--- Aplicacao/source/insert.php
+++ Aplicacao/source/insert.php
@@ -2,12 +2,20 @@
     include("conect.php");
 
     $nome = $_POST["name"];
+    $sobrenome = $_POST["lastname"];
     $email = $_POST["email"];
+    $telefone = $_POST["phone"];
+    $senha = $_POST["password"];
+    
+    $comando = $pdo -> prepare("INSERT INTO USUARIO (nome_usuario, sobrenome_usuario, email_usuario, telefone_usuario, senha_usuario) VALUES(:nome,:sobrenome,:email,:telefone,:senha})");
+    $comando->bindValue(":nome",$nome);                                   
+    $comando->bindValue(":sobrenome",$sobrenome);       
+    $comando->bindValue(":email",$email);
+    $comando->bindValue(":telefone",$telefone);                           
+    $comando->bindValue(":senha",$senha);
 
-    $comando = $pdo -> prepare("INSERT INTO usuario(nome_usuario, email_usuario) VALUES(:nome,:email)");  // Prepara o Comando de inserção
-    $comando->bindValue(":nome",$nome);                                     // Seleciona o dado que será inserido
-    $comando->bindValue(":email",$email);                                     // Seleciona o dado que será inserido
-    $comando->execute();                                                    // Executa o comando
+    $comando->execute();                                                   
 
-    header("Location:/cadastrar.html");
+    echo("sucesso");
+    //header("../pages/cadastrar.php");
 ?>
